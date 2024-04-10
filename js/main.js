0bb1f65396e240f3df10506b06d840cd02e08fec
Miguel Zacca <miguel_zacca@estudante.sesisenai.org.br>
2024-04-10 17:43:07 -0300

    Add dynamic id

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ const confirmBtn = document.querySelector("#confirmBtn");
 const cancelBtn = document.querySelector("#cancelBtn");
 const submitBtn = document.querySelector("#submitBtn");
 const resetBtn = document.querySelector("#resetBtn");
+const callInput = document.querySelector("#callInput");
+const screenId = document.querySelector("#screenId");
+
+const rand = () => Math.floor(Math.random() * 9999999);
+
+const id = rand();
+callInput.value = id;
+screenId.textContent = id.toString().padStart(7, "0");
 
 confirmBtn.addEventListener("click", () => {
   submitBtn.click();
